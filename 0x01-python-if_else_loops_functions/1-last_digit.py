25abac8afafe922445998419bfbaa9e694b1906a
Brian Omondi <omondiibrian00@gmail.com>
2023-06-06 17:30:43 +0300

    fx on code syntax

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ if number < 0:
 else:
     last_digit = number % 10
 
-print("The last digit of", number, "is", last_digit, end=" ")
+print("Last digit of", number, "is", last_digit, end=" ")
 
 if last_digit > 5:
     print("and is greater than 5")
