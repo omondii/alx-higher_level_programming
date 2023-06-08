efab88f8aca82bc2ca4ff773b4ea7778c6378f7f
Brian Omondi <omondiibrian00@gmail.com>
2023-06-08 21:53:29 +0300

    pycodestyle clean up

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if (len(sys.argv) -1) != 1:
+    if (len(sys.argv) - 1) != 1:
         print("{} argument".format(len(sys.argv) - 1), end="")
     else:
         print("{} argument".format(len(sys.argv) - 1), end="")
