d814c4993cdf613f8fa86de74d9387427e96a563
Brian Omondi <omondiibrian00@gmail.com>
2023-06-08 20:22:13 +0300

    removing f in print fnction

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print(f"{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
-    print(f"{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
 
-    print(f"{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
 
-    print(f"{} / {} = {}".format(a, b, div(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
