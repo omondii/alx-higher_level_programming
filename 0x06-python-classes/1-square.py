e68477970018e8d22431851b310dcf68e8cbe72a
Brian Omondi <omondiibrian00@gmail.com>
2023-06-28 01:16:27 +0300

    right documentation using goodle string documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-class Square:
-    """ Defines a square with a given size.
+""" Defines a square with a given size.
 
-        Attributes:
-              __size (int): The size of the square
-    """
+    Attributes:
+        __size (int): The size of the square
+"""
+
+
+class Square:
+    """Initializes the square object """
     def __init__(self, size):
-        """Initializes the square object """
         self.__size = size
