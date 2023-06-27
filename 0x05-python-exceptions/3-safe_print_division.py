c8e903ecf29fc87b44c43a331bce64b7b88f64aa
Brian Omondi <omondiibrian00@gmail.com>
2023-06-27 05:27:00 +0300

    Divide list elements one by one; catch type,index errors

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,6 +4,8 @@ safe_print_division(a, b) - divides 2 integers and prints the result
 a, b - integers to divide
 Return - if no error is received, inside result. Else, None
 """
+
+
 def safe_print_division(a, b):
     result = 0
     try:
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+list_division - divides element by element 2 lists
+my_list_1, my_list_2 - lists to divide
+list_length - can be bigger than the len of both lists
+"""
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    return_list = []
+
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result  = 0
+        finally:
+            return_list.append(result)
+    return return_list
