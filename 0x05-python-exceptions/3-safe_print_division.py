ae5ad90014b3632eb77ec8f3762928c1666318ff
Brian Omondi <omondiibrian00@gmail.com>
2023-06-27 05:16:10 +0300

    Divide 2 numbers; catch the div by 0 error

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+safe_print_division(a, b) - divides 2 integers and prints the result
+a, b - integers to divide
+Return - if no error is received, inside result. Else, None
+"""
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
