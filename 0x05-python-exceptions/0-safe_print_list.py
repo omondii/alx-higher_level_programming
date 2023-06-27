0b51aed7c937618291726711915619a4df3f8aed
Brian Omondi <omondiibrian00@gmail.com>
2023-06-27 04:52:21 +0300

    added except to habdle type and indes errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    if my_list is None:
-        my_list = []
+
     try:
         for i in range(x):
             print(my_list[i], end="")
             count += 1
+    except (TypeError, IndexError):
+        pass
     finally:
         print()
         return min(x, count)
