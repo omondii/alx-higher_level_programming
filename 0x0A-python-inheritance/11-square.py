110047754bfd32cde4e43da3e1dffb13af672fff
Brian Omondi <omondiibrian00@gmail.com>
2023-07-11 01:57:33 +0300

    str implemetation on square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Inherits from BaseGeometry
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square inherits from Rectangle
+    Params:
+       size - reps the 4 sides of a square
+    """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return(self.__size * self.__size)
+
+    def __str__(self):
+        return("[Square] {}/{}".format(self.__size, self.__size))
