0b6858520443216ca9f1a53b6bd31c89eadc7915
Brian Omondi <omondiibrian00@gmail.com>
2023-07-13 03:40:08 +0300

    a class tht defines a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/python3
+"""
+No modules imported
+"""
+
+
+class Student():
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize public attributes of the class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        return self.__dict__
