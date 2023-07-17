76ca5d38770ebc50136f6eedfb5f1fea47f0dfbb
Brian Omondi <omondiibrian00@gmail.com>
2023-07-17 19:56:09 +0300

    documented each function and class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,11 +7,19 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    Square inherits from rectangle
+    initialize with self to obtain all rectangle methods
+    params:
+         same to Rectangle but size == width/height
+    """
     def __init__(self, size, x=0, y=0, id=None):
+        """ Initialize the class. initialize rectangle using super """
         self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """ str implemetation to unify output """
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                   self.y, self.width))
 
