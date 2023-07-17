56bdda264577071a1447697c047e6dac4cabb09f
Brian Omondi <omondiibrian00@gmail.com>
2023-07-18 00:30:18 +0300

    pep8* fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,7 +23,6 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-
     @staticmethod
     def to_json_string(list_dictionaries):
         """ Returns the json rep of list_dictionaries """
