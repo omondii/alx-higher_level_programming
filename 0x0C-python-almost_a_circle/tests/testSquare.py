cfa300e714170f29a5cf10c700b354c96e55e236
Brian Omondi <omondiibrian00@gmail.com>
2023-07-18 19:57:15 +0300

    still on square tests

--- 0x0C-python-almost_a_circle/tests/testSquare.py
+++ 0x0C-python-almost_a_circle/tests/testSquare.py
@@ -11,7 +11,7 @@ class TestSquare(unittest.TestCase):
     @classmethod
     def setUp(self):
         self.s1 = Square(10, 4, 6, 5)
-        self.s2 = Square(8, 2)
+        self.s2 = Square(1, 2)
 
     @classmethod
     def tearDown(self):
@@ -24,7 +24,7 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(self.s1.x, 4)
         self.assertEqual(self.s1.y, 6)
 
-        self.assertEqual(self.s2.size, 8)
+        self.assertEqual(self.s2.size, 1)
 
     def test_typeerror(self):
         with self.assertRaises(TypeError) as se:
