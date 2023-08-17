8126cb0d2ef5064d25afa50eb2e44b8e44a4e9da
Brian Omondi <omondiibrian00@gmail.com>
2023-08-17 23:44:59 +0300

    pk and fk

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,8 @@
 -- id i unique, not null, auto generated and a pk
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       id INT NOT NULL AUTO_INCREMENT,
        name VARCHAR(256) NOT NULL,
+       PRIMARY KEY (id),
        UNIQUE (id)
 );
