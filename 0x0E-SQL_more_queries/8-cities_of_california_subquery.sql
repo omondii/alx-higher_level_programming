3c5460ac6c2629bff2bb6c26a7d04e557420a51b
Brian Omondi <omondiibrian00@gmail.com>
2023-08-28 14:29:40 +0300

    Cities by state

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,7 @@
 SELECT id, name
 FROM cities
 WHERE state_id = (
-      SELECT id FROM states
-      WHERE name = 'Califonia')
+      SELECT id
+      FROM states
+      WHERE name = 'California')
 ORDER BY id;
