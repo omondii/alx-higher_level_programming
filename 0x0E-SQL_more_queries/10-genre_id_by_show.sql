b63f5ab79dae01badd4b6c11a4465fc8bbe6225c
Brian Omondi <omondiibrian00@gmail.com>
2023-08-28 15:06:23 +0300

    correction on task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows
 -- that have at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM  tv_shows, tv_show_genres
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
+FROM  tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id > 0
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
