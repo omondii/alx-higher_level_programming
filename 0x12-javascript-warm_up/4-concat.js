3f3ce3af69b5bd94ce5f50fe7dc359023f2afab0
Brian Omondi <omondiibrian00@gmail.com>
2023-09-24 08:24:25 +0300

    An iterger or NaN

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 // Concatenate 2 strings passed as args into a sentence
-console.log(process.argv[2] + ' is ' + process.argv[3]);
+console.log(process.argv[2] + ' rm is ' + process.argv[3]);
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* a script that prints My number: <first argument converted in integer>
+   if the first argument can be converted to an integer */
+const arg = parseInt(process.argv[2]);
+
+if (isNaN(arg)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + arg);
+}
