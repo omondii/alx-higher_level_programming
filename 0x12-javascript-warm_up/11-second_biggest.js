2e27d495543b97853598a80aaf285b7dda0abd4d
Brian Omondi <omondiibrian00@gmail.com>
2023-09-24 14:19:13 +0300

    amended task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
-// searches the second biggest integer in the list of arguments.
+/* searches the second biggest integer in the list of arguments.
+ 'map' converts the rem args to int using parseInt. */
 const num = process.argv.slice(2).map(num => parseInt(num, 10));
 
 if (num < 2) {
-    console.log(0);
+  console.log(0);
 } else {
-    const numSort = num.sort((a, b) => b - a);
-    console.log(numSort[1]);
+  // Use a numeric comparison algo to sort the ints
+  const numSort = num.sort((a, b) => b - a);
+  console.log(numSort[1]);
 }
