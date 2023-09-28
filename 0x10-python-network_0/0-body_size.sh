c40d73ed783b9d07b5aef817546b884e0d07bd76
Brian Omondi <omondiibrian00@gmail.com>
2023-09-29 02:22:54 +0300

    fixed length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # A Bash script that takes in a URL, sends a request to that URL, and displays
 # the size of the body of the response
 
-curl -sI "$1" | grep "content-legth" | cut -d ' ' -f 2
+curl -sI "$1" | grep "content-length" | cut -d ' ' -f 2
