fb12c50b0b9e2a30a6489a0458371b045a8234fc
Brian Omondi <omondiibrian00@gmail.com>
2023-09-29 02:25:39 +0300

    removed family photos

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a request to that URL, and displays
-# the size of the body of the response
-
+# the size of the body of the responseclear
 curl -sI "$1" | grep "content-length" | cut -d ' ' -f 2
