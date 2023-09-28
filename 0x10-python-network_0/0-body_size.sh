d32de02a6c50f2db5b90f45f1149b7edaf2c1254
Brian Omondi <omondiibrian00@gmail.com>
2023-09-29 02:39:31 +0300

    Delete

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a request to that URL, and displays
-# the size of the body of the responseclear
+# A Bash script that takes in a URL, sends a request to that URL
 curl -sI "$1" | grep "content-length" | cut -d ' ' -f 2
