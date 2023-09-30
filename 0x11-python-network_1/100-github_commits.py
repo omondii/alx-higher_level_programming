e4098af68c5b3b78e9185c68b0d245a11f9a6740
Brian Omondi <omondiibrian00@gmail.com>
2023-09-30 04:39:36 +0300

    Time for an interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Imported modules;
+sys
+requests
+"""
+from sys import argv
+import requests
+
+
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
+
+    try:
+        for index in range(10):
+            print(f"{commits[index].get('sha')} {commits[index].get('commit').get('author').get('name')}")
+
+    except IndexError:
+        pass
