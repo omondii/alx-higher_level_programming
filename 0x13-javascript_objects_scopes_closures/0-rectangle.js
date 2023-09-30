1ef91327ca0ddcee21865a51e953369cd131ce8f
Brian Omondi <omondiibrian00@gmail.com>
2023-09-30 11:33:23 +0300

    Rec with empty instance

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* Defining an empty class using the class notatiion
+   classes can be considered to be special functions.
+*/
+
+class Rectangle {
+    constructor() {
+    }
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* Rectangle class with w&h constructors  */
+class Rectangle {
+    constructor (w, h) {
+	this.width = w;
+	this.height = h;
+    }
+}
+module.exports = Rectangle;
