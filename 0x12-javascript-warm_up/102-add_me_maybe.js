52837caeed75c71d79b9c2b7d2aad0c7ce9834dd
Brian Omondi <omondiibrian00@gmail.com>
2023-10-01 15:42:46 +0300

    increment

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // a function that increments and calls a function.
 function addMeMaybe (number, theFunction) {
-    number++;
-    theFunction(number);
+  number++;
+  theFunction(number);
 }
 
 exports.addMeMaybe = addMeMaybe;
