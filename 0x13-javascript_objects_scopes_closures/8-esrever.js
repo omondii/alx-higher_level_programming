25bfe04d714b924623359ecf42fd2c5869768e2e
Brian Omondi <omondiibrian00@gmail.com>
2023-10-01 12:47:11 +0300

    reverse a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Write a function that returns the reversed version of a list:
+
+exports.esrever = function (list) {
+    const newList = [];
+    // traverse list in reverse order
+    for (let i = list.length - 1; i >= 0; i--){
+	newList.push(list[i]);
+    }
+    return newList;
+}
