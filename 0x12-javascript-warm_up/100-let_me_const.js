a770f91c2276fad52e89302662cd69d3df5cf28e
Brian Omondi <omondiibrian00@gmail.com>
2023-10-01 15:11:22 +0300

    const or no const

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 // modifies myVar in an external function
-exports.mod = function mod(myVar) {
-  myVar = 333
-}
+myVar = 333;
--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // a function that executes x times a function.
 exports.callMeMoby = function (x, func) {
-    for (;x > 0; x--) { func(); }
+  for (;x > 0; x--) { func(); }
 };
