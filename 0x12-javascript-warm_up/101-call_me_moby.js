cf2690040709febd121279e876630db6db89ed3a
Brian Omondi <omondiibrian00@gmail.com>
2023-10-01 15:01:52 +0300

    callmemoby 2.1

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// a function that executes x times a function.
 exports.callMeMobby = function (x, func) {
-    for (; x > 0; x--) { func(); }
+    for (;x > 0; x--) { func(); }
 };
