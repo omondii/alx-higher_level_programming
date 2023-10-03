d2e3dedd53f152cebd79b59d5addccfc545775aa
Brian Omondi <omondiibrian00@gmail.com>
2023-10-03 17:44:35 +0300

    City relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Define state model """
+from relationship_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+class City(Base):
+    """ Define class state to create link to db """
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, autoincrement=True,
+                unique=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    staet_id = Column(Integer, ForeignKey('states.id'), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/python3
+""" state model """
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class State(Base):
+    """ class state to be linked to db table """
+    __tablename__ = 'states'
+
+    id = Column(Integer, nullable=False, autoincrement=True,
+                unique=True,primary_key=True)
+
+    name = Column(String(123), nullable=False)
+
+    cities = relationship('City', cascade='all, delete', backref='state')
