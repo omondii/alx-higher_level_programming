169750090569eeca4967a4fc8e67956331effab7
Brian Omondi <omondiibrian00@gmail.com>
2023-10-03 13:49:38 +0300

    all cities by state 1

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     cursor.execute('SELECT cities.name FROM cities '
                    'JOIN states '
                    'ON cities.state_id = states.id '
-                   'WHERE state.name = %s '
-                   'ORDER BY states.id ASC', (search))
+                   'WHERE states.name = %s '
+                   'ORDER BY states.id ASC', (search,))
     for row in cursor.fetchall():
         print(row)
