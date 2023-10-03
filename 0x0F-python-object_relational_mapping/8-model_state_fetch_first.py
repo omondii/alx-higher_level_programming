3f039fa1c9d2c7df5145aa3d7946b53cbeabfecd
Brian Omondi <omondiibrian00@gmail.com>
2023-10-03 19:08:37 +0300

    first state 2

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""
-Imported modules
-sys
-Base, State from model_state
-create_engine
-"""
-import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+""" List all state objects using sqlalchemy """
 
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}, pool_pre_ping=True')
-    # Create a session using the sessionmaker method
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm.session import sessionmaker, Session
+    from model_state import Base, State
+
+    username = '{}'.format(argv[1])
+    password = '{}'.format(argv[2])
+    db_name = '{}'.format(argv[3])
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name))
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query the db for first item only. Use the first() method
-    firstState = session.query(State).first()
-
-    # Print the query results
-    if firstState:
-        print(f'{firstState.id}: {firstState.name}')
+    result = session.query(State).order_by(State.id).first()
+    if result:
+        print('{}: {}'.format(result.id, result.name))
     else:
-        print("\n")
-
-    # Close the session
-    session.close()
+        print('Nothing')
