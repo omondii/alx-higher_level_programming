80bc218ee2b08304852eedb8bdea7a6f9e45d889
Brian Omondi <omondiibrian00@gmail.com>
2023-12-21 10:36:33 +0300

    Cor: semistd fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,8 +7,8 @@ const process = require('process');
 const content = process.argv[3];
 const file = process.argv[2];
 
-fs.writeFile(file, content, 'utf8', function(err, data) {
-    if (err) {
-        console.log(err);
-    }
+fs.writeFile(file, content, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
 });
